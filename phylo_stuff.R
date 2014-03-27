4e0d2fac0dd5233217184ce0294882505a5f5b33
jaeadie <jamie-e@hotmail.co.uk>
2014-03-27 22:31:09 +0000

    Update phylo_stuff.R

--- phylo_stuff.R
+++ phylo_stuff.R
@@ -17,32 +17,30 @@ d2<-dist.dna(x, model="K80")
 tr.nj<-nj(d)
 #plot the tree from the neighbour join algorithm 
 plot(tr.nj)
-
+#claculate the distortion
 dt.nj<-cophenetic(tr.nj)
-
+#pull out the taxa from (d) and assign them to dmat
 dmat<-as.matrix(d)
-
+#pull out the rownames of dmat and assign it to nms
 nms<-rownames(dmat)
-
+#update the cophenetic analysis with the rownames
 dt.nj<-dt.nj[nms, nms]
-
+#calcualte the new distances using as.dist
 dt.nj<-as.dist(dt.nj)
-#plot the residuals from the nj analysis
+#plot the residuals from the new distances with the originals subtracted to get the residuals
 plot(dt.nj-d,ylab="residuals", cex=0.5,main="NJ")
-
 abline(h=0,lty=3)
-
+#fit the data to the tree using pml
 fit <- pml(tr.nj,as.phyDat(x))
-
+#optimise this, then plot and set a random seed for the boostrap
 fit=optim.pml(fit,T)
-
 plot(fit)
-
 set.seed(8)
+
 #bootstrap the results 100 times to improve confidence in the position of each node in the tree
 bs<-bootstrap.pml(fit,bs=100,optNni=T)
 #plot the boostrapped tree in prefferred format, in this case the p format
 treeBS<-plotBS(fit$tree, type"p", bs)
-
+#perform a model test to find the most appropriate mutation model
 mt<-modelTest(as.phyDat(x),G=F,I=F)
 
